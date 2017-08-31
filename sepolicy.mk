a5649435be1f0442fe178c2e278d3723f568e84b
Marko Man <darkobas@gmail.com>
2017-08-31 13:08:36 +0200

    sepolicy-caf: Fix makefile
    Change-Id: Id6b6dafe3b3b21da5ac1d120f0ece95c52b15595

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,6 +1,23 @@
 # Board specific SELinux policy variable definitions
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+LOCAL_PATH:= $(call my-dir)
+BOARD_SEPOLICY_DIRS := \
+       $(BOARD_SEPOLICY_DIRS) \
+       $(LOCAL_PATH) \
+       $(LOCAL_PATH)/common \
+       $(LOCAL_PATH)/ssg \
+       $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       $(LOCAL_PATH)/test
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
+    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
+    $(LOCAL_PATH)/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
+    $(LOCAL_PATH)/private
+endif
