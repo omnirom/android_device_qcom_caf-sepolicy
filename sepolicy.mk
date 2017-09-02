6c786608d64358a56a6264cf97583c3e2b00793e
maxwen <max.weninger@gmail.com>
2017-09-02 12:57:54 +0200

    sepolicy-caf: update sepolicy.mk
    Change-Id: I74dd8b1d6e82f2ebf5915db0316082743a4cb1cb

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,6 +1,10 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy \
+    device/qcom/sepolicy/ssg \
+    device/qcom/sepolicy/common \
+    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/qcom/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/qcom/sepolicy/private
